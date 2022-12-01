f7b3a556c3a5316cab97a0b431225185cd9b5aae
Icy Cloud <i2cy@outlook.com>
2022-12-01 14:31:01 +0800

    updated SPI.py

---
+++ sources/modules/spi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: i2cy(i2cy@outlook.com)
+# Project: 9.3 地面接收机软件
+# Filename: spi
+# Created on: 2022/11/22
+
+from ch347api import CH347HIDDev
+
+
+class FPGA(CH347HIDDev):
+
+    def __init__(self):
+
+
